08e8d8faa9f54ce7e5f26f8298cca2fe96b76e9e
j222sang <wltkd96@naver.com>
2021-05-13 18:29:32 +0900

    레스토랑

---
+++ p55_restaurantChef/src/main/java/www/dream/com/di_sample/Restaurant.java
@@ -0,0 +1,18 @@
+package www.dream.com.di_sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Repository
+//기본생성자
+@NoArgsConstructor
+
+public class Restaurant {
+	//의존성 주입
+	@Autowired @Getter
+	private Chef chef;
+}
