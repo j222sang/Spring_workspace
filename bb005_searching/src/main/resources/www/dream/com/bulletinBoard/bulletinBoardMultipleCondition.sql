e7df4b124aee697122538e483de6824cd9fc060d
j222_sang <j222_sang@DESKTOP-4DA5CEM>
2021-05-31 21:20:29 +0900

    4

--- bb005_searching/src/main/resources/www/dream/com/bulletinBoard/bulletinBoardMultipleCondition.sql
+++
@@ -1,8 +0,0 @@
-create table s_post(
-   id				varchar2(4000)	primary key,	
-   board_id			number(9)		REFERENCES s_board(id),
-   writer_id		varchar2(10)	REFERENCES s_party(user_id),
-   title			varchar2(100),
-
-);
-
